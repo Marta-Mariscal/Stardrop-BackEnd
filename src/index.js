08bc402779ef5e5734e65d392d8be6ddebba1c3d
Marta Mariscal Velazquez <marta.mariscal@evolf.merkle.com>
2025-06-11 14:23:48 +0200

    index vercel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const garmentRouter = require("./routers/garment");
-const orderRouter = require("./routers/order");
+require("../db/mongoose");
+const userRouter = require("../routers/user");
+const garmentRouter = require("../routers/garment");
+const orderRouter = require("../routers/order");
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use(userRouter);
 app.use(garmentRouter);
 app.use(orderRouter);
 
-app.listen(port, () => {
-    console.log("Server is up on port " + port);
-});
+module.exports = app;
