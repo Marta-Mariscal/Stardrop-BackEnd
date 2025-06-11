0999099d4db7261a254327c15bb36f5f76508672
Marta Mariscal Velazquez <marta.mariscal@evolf.merkle.com>
2025-06-11 15:02:33 +0200

    arreglo index render

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ require("./db/mongoose");
 const userRouter = require("./routers/user");
 const garmentRouter = require("./routers/garment");
 const orderRouter = require("./routers/order");
-const wishlistRouter = require("./routers/wishlist");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use(userRouter);
 app.use(garmentRouter);
 app.use(orderRouter);
-app.use(wishlistRouter);
 
-module.exports = app;
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
+});
